88a18fda0c493dbbb5fd84caca039d71fb60e872
Miljan Misovic <mmiljan@hotmail.com>
2019-06-27 20:04:34 +0200

    code improvement

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/*
-Napraviti HTML stranu koja ima jedan input box tipa number, koji prima brojeve od 2-6 
-i jedan select koji ima 3 boje – plavu, crvenu i zelenu, i jedan button – run.
-
-Napisati program koji klikom na dugme “run” iscrtava za uneti ceo broj x iz opsega [2,6] 
-x^2 broj kvadrata u izabranoj boji, preko celog HTML. 
-Dijagonalu od leve gornje, do donje desne strane bojiti uvek u zutoj boji.
-*/
 
 var btn = document.querySelector("button");
 btn.addEventListener("click", draw);
@@ -15,22 +7,21 @@ function draw() {
     var divContainer = document.querySelector('#container');
     divContainer.innerHTML = ""
 
-    var size = document.querySelector("input").value;
     var selectedColor = document.querySelector("select").value;
+    var size = document.querySelector("input").value;
+    size = parseInt(size, 10);
+
+    document.querySelector("#pageTitle").style.color = selectedColor;
+    var percent = (100 - ((size - 1) * 1)) / size + "%"  // (100% container - (number of gaps) * 1%)) / size 
+    divContainer.style.gridTemplateColumns = "repeat(" + size + ", " + percent + ")";
+    divContainer.style.gridTemplateRows = "repeat(" + size + ", " + percent + ")";
+    divContainer.style.gridGap = "1%"
+    divContainer.style.borderColor = selectedColor
+
 
     if (size >= 2 && size <= 50) {
         for (var i = 0; i < size; i++) {
             for (var j = 0; j < size; j++) {
-                size = parseInt(size, 10);
-                // (100% container - (number of gaps) * 1%)) / size 
-                var percent = (100 - ((size - 1) * 1)) / size + "%"
-
-                divContainer.style.gridTemplateColumns = "repeat(" + size + ", " + percent + ")";
-                divContainer.style.gridTemplateRows = "repeat(" + size + ", " + percent + ")";
-                divContainer.style.gridGap = "1%"
-                divContainer.style.borderColor = selectedColor
-
-                document.querySelector("#pageTitle").style.color = selectedColor;
 
                 var divSquare = document.createElement("div");
                 divSquare.style.background = selectedColor;
