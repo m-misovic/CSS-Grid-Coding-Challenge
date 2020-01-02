b90d75c282cbb5f5ca1a140c10d2eb5dbcc43972
Miljan <48087520+m-misovic@users.noreply.github.com>
2020-01-02 14:10:57 +0100

    code update

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
-
-var btn = document.querySelector("button");
-btn.addEventListener("click", draw);
+var btn = document.querySelector('button');
+btn.addEventListener('click', draw);
 
 function draw() {
+	var divContainer = document.querySelector('#container');
+	divContainer.innerHTML = '';
 
-    var divContainer = document.querySelector('#container');
-    divContainer.innerHTML = ""
+	var selectedColor = document.querySelector('select').value;
+	var size = document.querySelector('input').value;
+	size = parseInt(size, 10);
 
-    var selectedColor = document.querySelector("select").value;
-    var size = document.querySelector("input").value;
-    size = parseInt(size, 10);
+	document.querySelector('#pageTitle').style.color = selectedColor;
+	var percent = (100 - (size - 1) * 1) / size + '%'; // (100% container - (number of gaps) * 1%)) / size
+	divContainer.style.gridTemplateColumns = 'repeat(' + size + ', ' + percent + ')';
+	divContainer.style.gridTemplateRows = 'repeat(' + size + ', ' + percent + ')';
+	divContainer.style.gridGap = '1%';
+	divContainer.style.borderColor = selectedColor;
 
-    document.querySelector("#pageTitle").style.color = selectedColor;
-    var percent = (100 - ((size - 1) * 1)) / size + "%"  // (100% container - (number of gaps) * 1%)) / size 
-    divContainer.style.gridTemplateColumns = "repeat(" + size + ", " + percent + ")";
-    divContainer.style.gridTemplateRows = "repeat(" + size + ", " + percent + ")";
-    divContainer.style.gridGap = "1%"
-    divContainer.style.borderColor = selectedColor
-
-
-    if (size >= 2 && size <= 50) {
-        for (var i = 0; i < size; i++) {
-            for (var j = 0; j < size; j++) {
-
-                var divSquare = document.createElement("div");
-                divSquare.style.background = selectedColor;
-                if (i === j) {
-                    divSquare.style.background = "yellow";
-                }
-                divSquare.style.borderRadius = "7px"
-                divContainer.appendChild(divSquare);
-            }
-        }
-    } else {
-        divContainer.innerHTML = `<div id="welcomeText">
+	if (size >= 2 && size <= 50) {
+		for (var i = 0; i < size; i++) {
+			for (var j = 0; j < size; j++) {
+				var divSquare = document.createElement('div');
+				divSquare.style.background = selectedColor;
+				if (i === j) {
+					divSquare.style.background = 'yellow';
+				}
+				divSquare.style.borderRadius = '7px';
+				divContainer.appendChild(divSquare);
+			}
+		}
+	} else {
+		divContainer.innerHTML = `<div id="welcomeText">
                                     <h3>Please, pick a number between 2 and 50!</h3>
                                   </div>`;
-        divContainer.style.gridTemplateColumns = "100%";
-        divContainer.style.gridTemplateRows = "100%";
-    }
+		divContainer.style.gridTemplateColumns = '100%';
+		divContainer.style.gridTemplateRows = '100%';
+	}
 }
